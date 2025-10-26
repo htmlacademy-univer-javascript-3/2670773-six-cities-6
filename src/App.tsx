d4fd6d7ec542552831ba9bb8ee98b9281b51e2b7
Artem_Mentolos <mentolosscg@yandex.ru>
2025-10-26 17:01:22 +0300

    third task routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router';
 import {MainPage} from './pages/MainPage';
+import {LoginPage} from './pages/LoginPage';
+import {FavoritesPage} from './pages/FavoritesPage';
+import {PropertyPage} from './pages/PropertyPage';
+import {NotFound} from './components/NotFound';
+import {ProtectedRoute} from './components/ProtectedRoute';
 
 type AppProps = {
-	offersCount?: number;
+  offersCount?: number;
 }
 
-const App: React.FC<AppProps> = ({offersCount = 312}) => <MainPage offersCount={offersCount}/>;
+const App: React.FC<AppProps> = ({offersCount = 312}) => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage offersCount={offersCount}/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route element={
+        <ProtectedRoute isAuthorized={false} redirectPath={'/login'}/>
+      }
+      >
+        <Route path="/favorites" element={<FavoritesPage/>}/>
+      </Route>
+      <Route path="/offer/:id" element={<PropertyPage/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
